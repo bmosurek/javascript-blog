d4f5b47b5d9a7bee5ac0d39c87a65930d8aeb649
bmosurek <68894569+bmosurek@users.noreply.github.com>
2021-04-30 23:18:15 +1000

    Fixed: script.js class active error

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
+"use strict";
 {
   const titleClickHandler = function (event) {
+    //display an article after clicking
     event.preventDefault();
     const clickedElement = this;
     console.log("Link was clicked!");
@@ -7,27 +9,36 @@
     /* [DONE] remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll(".titles a.active");
+
     for (let activeLink of activeLinks) {
       activeLink.classList.remove("active");
     }
 
-    /* [IN PROGRESS] add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
 
-    console.log("clickedElement:", clickedElement);
     clickedElement.classList.add("active");
+    console.log("clickedElement:", clickedElement);
+
+    /* [DONE] remove class 'active' from all articles */
+
+    const articles = document.querySelectorAll("article.active");
+
+    for (let article of articles) {
+      article.classList.remove("active");
+    }
 
     /* [DONE] get 'href' attribute from the clicked link */
 
     const articleSelector = clickedElement.getAttribute("href");
-    console.log(articleSelector);
 
-    /* [ERROR] find the correct article using the selector (value of 'href' attribute) */
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
 
     const targetArticle = document.querySelector(articleSelector);
-    console.log(targetArticle);
+    console.log("targetArticle", targetArticle);
 
     /* add class 'active' to the correct article */
     targetArticle.classList.add("active");
+    console.log("active article", targetArticle);
   };
 
   const links = document.querySelectorAll(".titles a");
